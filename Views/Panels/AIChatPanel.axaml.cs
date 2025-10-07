9e284e808a7019f7fbdcda5a362a8ff448921a7d
Your Name <Your Name>
2025-10-07 09:23:31 +0800

    创建 AI 聊天设置面板（任务22）

---
+++ Views/Panels/AIChatPanel.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ConfigButtonDisplay.Views.Panels;
+
+public partial class AIChatPanel : UserControl
+{
+    public AIChatPanel()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
